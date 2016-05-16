9e619bfb596cc52ffeefdd0041bf426b06365f31
playersun <sunhui1101@gmail.com>
2016-05-16 09:16:00 +0800

    修改下拉更新，上拉分页

--- channel_localization_management/client/platforms/android/assets/www/js/page.choose.js
+++ channel_localization_management/client/platforms/android/assets/www/js/page.choose.js
@@ -1,16 +1,28 @@
-(function(){
+(function($){
   globle.page.choose = {
     id: "choose",
     choosedVal:"",
-    init: function(){
+    pullDownGeneratedCount: 0,
+    pullUpGeneratedCount: 0,
+    listSelector:"div.pull-demo-page ul.ui-listview",
+    init: function(obj){
       var self = this;
       self.isr_choose = $("#isr_choose").on("change",function(ent){
         self.choosedVal = ent.target && ent.target.value;
         console.log(self.choosedVal);
         $.mobile.changePage("#" + self.choosedVal);
       });
+
+      self.scroll = new iScroll('wrapper', {
+    		scrollbarClass: 'myScrollbar', /* 重要样式 */
+    		useTransition: false
+    	});
     }
   };
 
-  globle.page.choose.init();
-})();
+  // Set-up jQuery event callbacks
+  $(document).delegate("#choose", "pageinit",
+    function bindPullPagePullCallbacks(event) {
+      globle.page.choose.init(event.target);
+    });
+}(jQuery));
