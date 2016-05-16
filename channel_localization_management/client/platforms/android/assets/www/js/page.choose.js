728caad447c3f413cd0b8e9fa035d960144516d3
playersun <sunhui1101@gmail.com>
2016-05-16 18:47:00 +0800

    修改为jquery.mobile.iscrollview

--- channel_localization_management/client/platforms/android/assets/www/js/page.choose.js
+++ channel_localization_management/client/platforms/android/assets/www/js/page.choose.js
@@ -5,24 +5,26 @@
     pullDownGeneratedCount: 0,
     pullUpGeneratedCount: 0,
     listSelector:"div.pull-demo-page ul.ui-listview",
-    init: function(obj){
+    init: function(){
       var self = this;
       self.isr_choose = $("#isr_choose").on("change",function(ent){
         self.choosedVal = ent.target && ent.target.value;
         console.log(self.choosedVal);
         $.mobile.changePage("#" + self.choosedVal);
       });
-
-      self.scroll = new iScroll('wrapper', {
-    		scrollbarClass: 'myScrollbar', /* 重要样式 */
-    		useTransition: false
-    	});
     }
   };
 
   // Set-up jQuery event callbacks
   $(document).delegate("#choose", "pageinit",
     function bindPullPagePullCallbacks(event) {
-      globle.page.choose.init(event.target);
+      globle.page.choose.init();
+
+      $(".iscroll-wrapper", this).bind( {
+        "iscroll_onpulldown" : function(){},
+        "iscroll_onpullup"   : function(){}
+        });
+
+      globle.page.choose.scrollView = $(".iscroll-wrapper").data("mobile-iscrollview");
     });
 }(jQuery));
